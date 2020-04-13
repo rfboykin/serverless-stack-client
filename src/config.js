8148c5f5f04583dcb0cfa407700f93de52e0bda7
Rob Boykin <rboykin@mintel.com>
2020-04-12 20:07:22 -0500

    added netlify and config

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-rboykin-uploads"
+    BUCKET: "notes-app-2-api-dev-serverlessdeploymentbucket-sqk3s94z7xnt"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://v1s71e3vxd.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://notes-api.rboyk.in/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_IvPyY3Ja5",
-    APP_CLIENT_ID: "9qq0pd8c60er4cq06demhc0it",
-    IDENTITY_POOL_ID: "us-east-1:43b87d69-8136-4f26-ae19-5a963da70f07"
+    USER_POOL_ID: "us-east-1_qP0Q8DUNj",
+    APP_CLIENT_ID: "55tutoqprkql3f9jkm73hdpjri",
+    IDENTITY_POOL_ID: "us-east-1:45ecfea8-4049-4e72-832f-c6f78976b89d"
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-rboykin-uploads"
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-14eo9qkny2c62"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://v1s71e3vxd.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://notes-api.rboyk.in/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_IvPyY3Ja5",
-    APP_CLIENT_ID: "9qq0pd8c60er4cq06demhc0it",
-    IDENTITY_POOL_ID: "us-east-1:43b87d69-8136-4f26-ae19-5a963da70f07"
+    USER_POOL_ID: "us-east-1_xmlavAtL2",
+    APP_CLIENT_ID: "1p5n3mfimrc8acel47rugs99k1",
+    IDENTITY_POOL_ID: "us-east-1:f888c3be-35cc-4f64-bbbc-b0dfe0c140aa"
   }
 };
 
